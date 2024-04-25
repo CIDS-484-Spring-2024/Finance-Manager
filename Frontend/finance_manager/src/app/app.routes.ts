6a2a14d2a5984c45a269032b5f8848c324fc3670
Randy Franzmeier <randyfranzmeier@gmail.com>
2024-04-25 13:02:29 -0500

    created 404 error page and added it as an unknown route.

--- Frontend/finance_manager/src/app/app.routes.ts
+++ Frontend/finance_manager/src/app/app.routes.ts
@@ -1,12 +1,13 @@
-import { Routes } from '@angular/router';
-import { LoginComponent } from "./login/login.component";
-import { SignupComponent } from "./signup/signup.component";
+import {Routes} from '@angular/router';
+import {LoginComponent} from "./login/login.component";
+import {SignupComponent} from "./signup/signup.component";
 import {LandingPageComponent} from "./landing-page/landing-page.component";
 import {UserDetailsComponent} from "./userFinanceForm/user-details.component";
 import {AboutUsComponent} from "./about-us/about-us.component";
 import {userinfoGuard} from "./userinfo.guard";
 import {leadGuardGuard} from "./lead-guard.guard";
 import {FinGraphComponent} from "./fin-graph/fin-graph.component";
+import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
 
 
 export const routes: Routes = [{path: "login", component: LoginComponent},
@@ -14,5 +15,7 @@ export const routes: Routes = [{path: "login", component: LoginComponent},
                                {path:"my-info", component: UserDetailsComponent, canActivate: [userinfoGuard]},
                                {path:"AboutUs", component: AboutUsComponent},
                                {path: "graph", component: FinGraphComponent},
-                               {path:"", component: LandingPageComponent}
+  {path: "", component: LandingPageComponent},
+  //when nothing matches:
+  {path: "**", component: PageNotFoundComponent}
 ];
